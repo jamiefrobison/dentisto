e4ccdb504ddd9b2bbb74870f880b223c98c77df0
matthewxfischer <matthewxfischer@gmail.com>
2017-02-12 21:16:32 +0200

    testing clearDB

--- server/database/config.js
+++ server/database/config.js
@@ -2,8 +2,8 @@ var Sequelize = require('sequelize');
 var mysql = require('mysql');
 
 // connecting database with my local host
-var sequelize = new Sequelize('dentisto', 'root', 'L.I.M.S', {
-  host: 'localhost',
+var sequelize = new Sequelize('heroku_6418bb6b0a5b45a', 'b084ae3e58aa89', 'becb6734', {
+  host: 'us-cdbr-iron-east-04.cleardb.net',
   dialect: 'mysql'
 });
 module.exports = sequelize;
