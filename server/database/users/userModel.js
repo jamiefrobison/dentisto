94947f3f1a5535c79d7aa28fe9f1fcc0974f6c1c
matthewxfischer <matthewxfischer@gmail.com>
2017-02-13 13:17:34 +0200

    userModel done

--- server/database/users/userModel.js
+++ server/database/users/userModel.js
@@ -2,11 +2,29 @@ var db = require('../config.js');
 var Sequelize = require('sequelize');
 
 var User = db.define('User', {
-  username: Sequelize.STRING,
-  email: Sequelize.STRING,
-  password: Sequelize.STRING
+  id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: Sequelize.STRING, allowNull: false },
+  email: { type: Sequelize.STRING, allowNull: false, unique: true },
+  password: { type: Sequelize.STRING, allowNull: false },
+  phoneNumber: { type: Sequelize.STRING, field: 'phone_number', allowNull: false },
+  gender: { type: Sequelize.STRING, allowNull: false }
 });
 
-db.sync();
+var Student = db.define('Student', {
+  id: { type: Sequelize.INTEGER, primaryKey: true },
+  university: { type: Sequelize.STRING, allowNull: false }
+});
+
+var Patient = db.define('Patient', {
+  id: { type: Sequelize.INTEGER, primaryKey: true },
+  address: { type: Sequelize.STRING, allowNull: false }
+});
+
+Student.belongsTo(User, {foreignKey: 'id'});
+Patient.belongsTo(User, {foreignKey: 'id'});
+
+db.sync({
+  force: true
+});
 
 module.exports = User;
