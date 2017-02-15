11665bb59a1b21b9fc4b3d0b8ae0f6207e94c85e
matthewxfischer <matthewxfischer@gmail.com>
2017-02-15 15:31:34 +0200

    (fix)signupAuth

--- client/sign/sign.js
+++ client/sign/sign.js
@@ -2,5 +2,7 @@ angular.module('dentisto.sign', [])
 .controller('signCtlr', function ($scope,$http,$state,Auth) {
   if(Auth.isAuth()){
   	$state.go('home.profile')
+  } else{
+  	$state.go('sign.login')
   }
 });
--- client/signup/signup.js
+++ client/signup/signup.js
@@ -1,36 +1,3 @@
-// angular.module('dentisto.signup', [])
-// .controller('signupClr', function ($scope,$http,$state,Auth) {
-//   // if(Auth.isAuth()){
-//   //   $state.go('home');
-//   // } else{
-//     $scope.info= {};
-//     $scope.SendData = function () {
-//       var data = {
-//         name: $scope.info.name,
-//         email:$scope.info.email,
-//         password: $scope.info.password,
-//         gender:$scope.info.gender,
-//         phoneNumber:$scope.info.phoneNumber,
-//         address:$scope.info.address,
-//         university:$scope.info.university
-//         };
-//         console.log(data);
-
-//         var config = {
-//           headers : {
-//             'Content-Type': 'application/json'
-//           }
-//         }
-//         Auth.register(data, config);   
-//     };
-//   //}
-  
-
-// })
-
-
-
-
 angular.module('dentisto.signup', [])
 .controller('signupClr', function ($scope,$http) {
   $scope.info= {};
@@ -56,7 +23,6 @@ angular.module('dentisto.signup', [])
             university:$scope.info.university
           };
 
-          console.log(data);
            var config = {
             headers : {
               'Content-Type': 'application/json'
