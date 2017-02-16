5dcf9b88dfdaf033e96dc4551caf3a79ad8d1951
matthewxfischer <matthewxfischer@gmail.com>
2017-02-16 18:52:49 +0200

    (bug)fixing search

--- client/lookup/lookUp.js
+++ client/lookup/lookUp.js
@@ -1,37 +1,59 @@
 angular.module('dentisto.studentLookup', [])
 
+.controller('lookUpCtrl', function ($scope,$http,$interval,Auth) {
+	$scope.cases = []; // for all cases..
+	$scope.array=[]; // for data from search function ..
+	$scope.default=true; // to display the default data ..
+	$scope.s=false; // to display the search data ..
 
-.factory('StudentLookUp', function($http){
-	var cases={};
 
-	cases.lookUpCases= function(){
+		if(Auth.getUser()==="student"){
+			$scope.type = true;
+		} else {
+			$scope.type = false;
+		}	
+	
+    $http.get('/lookup') //to get the All cases from the server...
+    .success(function (data) {
+    	$scope.cases=data;
+    })
+    .error(function (data) {
+    	alert("error to get data :(");
+    });
 
-	};
 
-	cases.searchCases= function(){
+	$scope.searchFun=function(){ //search function ...
+		$scope.s=true;
+		$scope.default=false;
+		if($scope.search!==""){
+			$scope.array=$scope.cases.reduce(function(acc,obj){
+				for(var k in obj){
+					if(obj[k].indexOf($scope.search)>-1){
+						acc.push(obj)
+					}
+				}
+				return acc;
+			},[])
+		}else{
+			$scope.default=true;
+			$scope.s=false;	
+		}
 
-	};
+	}
 
-	return cases;
-})
-.controller('lookUpCtrl', function ($scope, StudentLookUp) {
-	$scope.patientCases=[{patientName:'dan', caseName:'rrr', caseDesc:'i felt kljsdfg'},
-	{patientName:'sam', caseName:'sdfg', caseDesc:'i felt kljsdfg'},
-	{patientName:'man', caseName:'sdfg', caseDesc:'i felt kljsdfg'},
-	{patientName:'fan', caseName:'fgh', caseDesc:'i felt kljsdfg'},
-	{patientName:'pan', caseName:'sdg', caseDesc:'i felt kljsdfg'},
-	{patientName:'kan', caseName:'dfhfhg', caseDesc:'i felt kljsdfg'},
-	{patientName:'sad', caseName:'dfgh', caseDesc:'i felt kljsdfg'},
-	{patientName:'bad', caseName:'ertyty', caseDesc:'i felt kljsdfg'}];
 
-  // StudentLookUp.lookUpCases().then(function(cases){
-  // 		$scope.patientCases=cases;
-  // }).catch(function(error){
-  // 	console.error(error);
-  // })
 
-  // $scope.search = function(){
 
-  // 	StudentLookUp.searchCases
-  // };
+   //try this one...
+
+   // $interval(function () {
+   // 	$http.get('/lookup') 
+   // 	.success(function (data) {
+   // 		$scope.cases=data; 
+   // 	})
+   // 	.error(function (data) {
+   // 		console.log("error to get data :(");
+   // 	});
+   // }, 10000);
+
 });
