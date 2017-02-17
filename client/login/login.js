42920756baf603451fa7c703c1b50c1c7edaa43c
matthewxfischer <matthewxfischer@gmail.com>
2017-02-17 22:21:07 +0200

    (fix)modifyCancelToGoToHomePage,UnionUserExperinceAndDeleteUnusedItems

--- client/login/login.js
+++ client/login/login.js
@@ -1,7 +1,11 @@
 angular.module('dentisto.logApp', [])
 .controller('loginCrl', function ($scope, $state, Auth) {
-        $scope.log=false;
         $scope.user={};
+        
+        $scope.backTohome= function(){
+          $state.go('sign.slideBar');            
+        };
+        
         $scope.SendData = function () {
            if($scope.user.email&&$scope.user.password){
             var data = {
