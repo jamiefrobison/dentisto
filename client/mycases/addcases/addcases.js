9a0166511107bc8991f256bef5c6c8d6445a5768
matthewxfischer <matthewxfischer@gmail.com>
2017-02-18 03:26:55 +0200

    (fix)updateCasesImmediatelyAfterDeleteAndAddOfCase,displayDifferentContentForExistedAndUnexistedCasesAndSomeCSSmodification

--- client/mycases/addcases/addcases.js
+++ client/mycases/addcases/addcases.js
@@ -1,8 +1,8 @@
 angular.module('dentisto.addcases', ['ui.bootstrap'])
 
 
-.controller('addUserCaseCtrl',function($scope, Auth,$http, $state, $modalInstance) {
-  console.log('1111111111111111')
+.controller('addUserCaseCtrl',function($scope, Auth,$rootScope, $modalInstance) {
+
   $scope.ucase= {};
   $scope.close = function () {
   	$modalInstance.dismiss('cancel');
@@ -12,5 +12,6 @@ angular.module('dentisto.addcases', ['ui.bootstrap'])
     console.log('add case has been invoked')
     Auth.addcase($scope.ucase);
     $scope.close();
+    $rootScope.initializeCases();
   };  
 });
